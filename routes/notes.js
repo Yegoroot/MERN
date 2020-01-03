80f8954cea196879944d267866252678a3ddfb33
Yegor Kirsanov <kirsanov.yegor@gmail.com>
2020-01-04 00:50:11 +0300

    Router

---
+++ routes/notes.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (req, res) => {
+	res.status(200).json({success: true, msg: 'Show all notes'})
+})
+
+router.get('/:id', (req, res) => {
+	res.status(200).json({success: true, msg: `Show note ${req.params.id}`})
+})
+
+router.post('/', (req, res) => {
+	res.status(200).json({success: true, msg: 'Create new note'})
+})
+
+router.put('/:id', (req, res) => {
+	res.status(200).json({success: true, msg: `Update note ${req.params.id}`})
+})
+
+router.delete('/:id', (req, res) => {
+	res.status(200).json({success: true, msg: `Delete note ${req.params.id}`})
+})
+
+module.exports = router
