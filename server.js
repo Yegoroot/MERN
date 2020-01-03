3682d36ff848c1d84e2f4368c860cb47f8d645c3
Yegor Kirsanov <kirsanov.yegor@gmail.com>
2020-01-04 00:50:11 +0300

    Router

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
 const express = require('express')
 const dotenv = require('dotenv')
 
+// Route files
+const notes = require('./routes/notes')
+
 // load en vars
-dotenv.config({path: './config/config.env'})
+dotenv.config({ path: './config/config.env' })
 
 const app = express()
 
+// Mount routers
+app.use('/api/v1/notes', notes)
+
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`server running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`))
-
-// let notes = [
-// 	{ id: 1, date: 'text 1' },
-// 	{ id: 2, date: 'text 2' },
-// 	{ id: 3, date: 'text 3' }
-// ]
+app.listen(
+	PORT,
+	console.log(
+		`server running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`
+	)
+)
