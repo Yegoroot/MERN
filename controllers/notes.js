44582b8515ccf8a319ac18e15e9adfeb12318ac8
Yegor Kirsanov <kirsanov.yegor@gmail.com>
2020-01-06 17:50:18 +0300

    controllers

--- controllers/notes.js
+++ controllers/notes.js
@@ -3,15 +3,33 @@ const Note = require('../model/Note')
 // @desc    Get all notes
 // @route   GET /api/v1/notes
 // @access  Public
-exports.getNotes = (req, res, next) => {
-	res.status(200).json({success: true, msg: 'Show all notes'})
+exports.getNotes = async (req, res, next) => {
+
+	try {
+		const notes = await Note.find()
+		res.status(200).json({success: true, count: notes.length, data: notes})
+	} catch (error) {
+		
+		res.status(400).json({success: false})
+	}
 }
 
 // @desc    Get single note
 // @route   GET /api/v1/notes/:id
 // @access  Public
-exports.getNote = (req, res, next) => {
-	res.status(200).json({success: true, msg: `Show note ${req.params.id}`})
+exports.getNote =  async (req, res, next) => {
+	try {
+		const note = await Note.findById(req.params.id)
+		res.status(200).json({success: true, data: note})
+		
+		if(!note) {
+			return res.status(400).json({success: false,})
+		}
+
+	} catch (error) {
+		res.status(400).json({success: false,})
+	}
+	
 }
 
 // @desc    Create note
@@ -32,13 +50,40 @@ exports.createNote = async (req, res, next) => {
 // @desc    Update note
 // @route   PUT /api/v1/notes/:id
 // @access  Private
-exports.updateNote = (req, res, next) => {
-	res.status(200).json({success: true, msg: `Update note ${req.params.id}`})
+exports.updateNote = async (req, res, next) => {
+	try {
+		
+		const note = await Note.findByIdAndUpdate(req.params.id, req.body, {
+			new: true,
+			runValidators: true
+		})
+	
+		if (!note) {
+			return res.status(400).json({success: false})
+		}
+		
+		return res.status(200).json({success: true, data: note})
+	} catch (error) {
+		
+		return res.status(400).json({success: false})
+	}
 }
 
 // @desc    Delete note
 // @route   DELETE /api/v1/notes/:id
 // @access  Private
-exports.deleteNote = (req, res, next) => {
-	res.status(200).json({success: true, msg: `Delete note ${req.params.id}`})
+exports.deleteNote = async (req, res, next) => {
+	try {
+		
+		const note = await Note.findByIdAndDelete(req.params.id)
+	
+		if (!note) {
+			return res.status(400).json({success: false})
+		}
+
+		return res.status(200).json({success: true, data: {}})
+	} catch (error) {
+		
+		return res.status(400).json({success: false})
+	}
 }
