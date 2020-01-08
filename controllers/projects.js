c547a60bfd3e3c21e830b274557060a50db87ba8
Yegor Kirsanov <kirsanov.yegor@gmail.com>
2020-01-09 00:23:42 +0300

    advanced filter

--- controllers/projects.js
+++ controllers/projects.js
@@ -1,92 +1,75 @@
 const ErrorResponse = require('../utils/errorResponse')
-
+const asyncHandler = require('../middleware/async')
 const Project = require('../models/Projects')
 
 // @desc    Get all projects
 // @route   GET /api/v1/projects
 // @access  Public
-exports.getNotes = async (req, res, next) => {
-	try {
-		
-		const projects = await Project.find()    
-		res.status(200).json({success: true, count: projects.length, data: projects})
-	} catch (error) {
+exports.getNotes = asyncHandler(async (req, res, next) => {
+	let query
 
-		next(error) // тут отработает app.use(errorHandlrer) middleware который мы подключили в server.js
-	}
-}
+	let queryStr = JSON.stringify(req.query)
+
+	queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`)
+	
+	query = Project.find(JSON.parse(queryStr)) 
+
+	const projects = await query  
+
+	res.status(200).json({success: true, count: projects.length, data: projects})
+
+})
 
 // @desc    Get single project
 // @route   GET /api/v1/projects/:id
 // @access  Public
-exports.getNote =  async (req, res, next) => {
-	try {
-		const project = await Project.findById(req.params.id)
-		
-		if(!project) {	
-			return	next(new ErrorResponse(`Project not found with of id ${req.params.id}`, 404))
-		}
-		
-		res.status(200).json({success: true, data: project})
-	} catch (error) {
+exports.getNote =  asyncHandler(async (req, res, next) => {
 
-		next(error) // тут отработает app.use(errorHandlrer) middleware который мы подключили в server.js
+	const project = await Project.findById(req.params.id)
+		
+	if(!project) {	
+		return	next(new ErrorResponse(`Project not found with of id ${req.params.id}`, 404))
 	}
-	
-}
+		
+	res.status(200).json({success: true, data: project})
+})
 
 // @desc    Create project
 // @route   POST /api/v1/projects/:id
 // @access  Private
-exports.createNote = async (req, res, next) => {
-	// console.log(req.body)
-	try {
-		const project = await Project.create(req.body)
-		res.status(201).json({success: true, data: project})
-	} catch (error) {
+exports.createNote = asyncHandler(async (req, res, next) => {
 
-		next(error) // тут отработает app.use(errorHandlrer) middleware который мы подключили в server.js
-	}
-
-}
+	const project = await Project.create(req.body)
+	res.status(201).json({success: true, data: project})
+})
 
 // @desc    Update project
 // @route   PUT /api/v1/projects/:id
 // @access  Private
-exports.updateNote = async (req, res, next) => {
-	try {
-		
-		const project = await Project.findByIdAndUpdate(req.params.id, req.body, {
-			new: true,
-			runValidators: true
-		})
+exports.updateNote = asyncHandler(async (req, res, next) => {
+
+	const project = await Project.findByIdAndUpdate(req.params.id, req.body, {
+		new: true,
+		runValidators: true
+	})
 	
-		if (!project) {
-			return	next(new ErrorResponse(`Project not found with of id ${req.params.id}`, 404))
-		}
-		
-		return res.status(200).json({success: true, data: project})
-	} catch (error) {
-		
-		next(error) // тут отработает app.use(errorHandlrer) middleware который мы подключили в server.js
+	if (!project) {
+		return	next(new ErrorResponse(`Project not found with of id ${req.params.id}`, 404))
 	}
-}
+		
+	return res.status(200).json({success: true, data: project})
+})
 
 // @desc    Delete project
 // @route   DELETE /api/v1/projects/:id
 // @access  Private
-exports.deleteNote = async (req, res, next) => {
-	try {
-		
-		const project = await Project.findByIdAndDelete(req.params.id)
-	
-		if (!project) {
-			return	next(new ErrorResponse(`Project not found with of id ${req.params.id}`, 404))
-		}
+exports.deleteNote = asyncHandler(async (req, res, next) => {
 
-		return res.status(200).json({success: true, data: {}})
-	} catch (error) {
-		
-		next(error) // тут отработает app.use(errorHandlrer) middleware который мы подключили в server.js
+	const project = await Project.findByIdAndDelete(req.params.id)
+	
+	if (!project) {
+		return	next(new ErrorResponse(`Project not found with of id ${req.params.id}`, 404))
 	}
-}
+
+	return res.status(200).json({success: true, data: {}})
+})
