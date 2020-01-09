b556b89e4999358e380728077506cffbf8798744
Yegor Kirsanov <kirsanov.yegor@gmail.com>
2020-01-09 06:17:09 +0300

    get coursec by ID project

--- routes/categories.js
+++ routes/categories.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const router = express.Router()
+const router = express.Router({mergeParams: true})
 
 const { getCategories, getCategory, createCategory, updateCategory, deleteCategory } = require('../controllers/categories')
 
