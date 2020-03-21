c61b244b9c99a9a14a74f27fd10b86d20f47ab94
Yegor Kirsanov <kirsanov.yegor@gmail.com>
2020-03-21 17:48:52 +0300

    SIgn & Get JSON Web Token

--- controllers/auth.js
+++ controllers/auth.js
@@ -15,8 +15,11 @@ exports.register = asyncHandler(async (req, res, next) => {
 		name, email, password, role
 	})
 	
+	// Create token
+	const token = user.getSignedJwtToken()
+
 	res.status(200).json({
-		success: true
+		success: true, token
 	})
 
 })
