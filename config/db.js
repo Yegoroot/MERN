5a2cc83520913eae27cff1213f1fed46630cc277
Yegor Kirsanov <kirsanov.yegor@gmail.com>
2020-08-29 05:55:01 +0300

    url mongo depends on production mode

--- config/db.js
+++ config/db.js
@@ -1,12 +1,17 @@
 const mongoose = require('mongoose')
+const URL = process.env.NODE_ENV === 'production'
+	? process.env.MONGO_URL
+	: 'mongodb://localhost:27017/madinah'
 
 const connectDB = async () => {
-	const conn = await mongoose.connect(process.env.MONGO_URI, {
-		useNewUrlParser: true,
-		useCreateIndex: true,
-		useFindAndModify: false,
-		useUnifiedTopology: true
-	})
+	const conn = await mongoose
+		.connect(`${URL}`,
+			{
+				useNewUrlParser: true,
+				useCreateIndex: true,
+				useFindAndModify: false,
+				useUnifiedTopology: true
+			})
 
 	console.log(`MongoDB connected ${conn.connection.host}`.cyan.brightRed.underline)
 }
