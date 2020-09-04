7060018437e96dd5173ae23012099d8d5d93e7af
Yegor Kirsanov <kirsanov.yegor@gmail.com>
2020-09-04 13:19:36 +0300

    delete information section

--- information/StructureProject/middleware/logger.js
+++
@@ -1,9 +0,0 @@
-// @desc    Logs request console
-// на данный момент этот кастомный не используется а используеться morgan
-
-const logger = (req, res, next) => {
-	console.log(`${req.method} ${req.protocol}://${req.get('host')}${req.originalUrl}`)
-	next()
-}
-
-module.exports = logger
